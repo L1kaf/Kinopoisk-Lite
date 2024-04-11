b3c23324732f2bfded7195037e73ee5cda739d39
Pavel Buryka <l1kaf@yandex.ru>
2024-04-11 20:51:49 +0300

    add destroy and update Category

--- src/Services/CategoryService.php
+++ src/Services/CategoryService.php
@@ -28,4 +28,41 @@ class CategoryService
             );
         }, $categories);
     }
+
+    public function delete(int $id): void
+    {
+        $this->db->delete('categories', ['id' => $id]);
+    }
+
+    public function store(string $name): int
+    {
+        return $this->db->insert('categories', [
+            'name' => $name,
+        ]);
+    }
+
+    public function find(int $id): ?Category
+    {
+        $category = $this->db->first('categories', ['id' => $id]);
+
+        if (! $category) {
+            return null;
+        }
+
+        return new Category(
+            id: $category['id'],
+            name: $category['name'],
+            createdAt: $category['created_at'],
+            updatedAt: $category['updated_at'],
+        );
+    }
+
+    public function update(int $id, string $name): void
+    {
+        $this->db->update('categories', [
+            'name' => $name,
+            ], [
+                'id' => $id
+            ]);
+    }
 }
